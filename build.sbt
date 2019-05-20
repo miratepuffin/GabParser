cc6cfa1d6d4f2b63b6f1b18da3951f48902a4a42
miratepuffin <piratemuffin@hotmail.co.uk>
2019-05-20 16:03:36 +0100

    initial push

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "GabParser"
+
+version := "0.1"
+
+scalaVersion := "2.11.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0" % "provided"
+// https://mvnrepository.com/artifact/io.spray/spray-json
+// https://mvnrepository.com/artifact/io.spray/spray-json
+libraryDependencies += "io.spray" %% "spray-json" % "1.3.0"
+
---
+++ src/main/scala/GabParser.scala
@@ -0,0 +1,52 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import spray.json._
+import GabJsonProtocol.GabPostJsonFormat
+object GabParser extends App{
+  val conf = new SparkConf()
+  val sc = new SparkContext(conf)
+  sc.setLogLevel("ERROR")
+  val rawposts = sc.textFile("/data/gabFull")
+  rawposts.map(raw => {
+    try{
+      val post = raw
+                  .drop(raw.indexOf("\"b'")+3)
+                  .dropRight(3)
+                  .replaceAll("\\","")
+                  .parseJson.convertTo[GabPost]
+      val id = post.id.get
+      val time = post.created_at.get
+      var userID = -1
+      //post.user match {
+      //  case Some(user) => userID = user.id
+      //  case None =>
+     // }
+      var topicID = ""
+     // post.topic match {
+     //   case Some(topic) => topicID = topic.id
+     //   case None =>
+      //}
+      var parentID = -1L
+      var parentUserID = -1
+     // post.parent match {
+     //   case Some(parent) => {
+     //     parentID = parent.id.get
+      //    parent.user match {
+      //      case Some(user) =>  parentUserID = user.id
+     //       case None =>
+     //     }
+     //   }
+    //    case None =>
+    //  }
+      s"$time;$id;$userID;$topicID;$parentID;$parentUserID"
+    }
+    catch {
+      case e:Exception => "failed"
+    }
+  }).filter(x => !x.equals("failed")).saveAsTextFile("gabFinal")
+}
+
+object GabEntityType extends Enumeration {
+  val post : Value = Value("post")
+  val user : Value = Value("user")
+  val topic: Value = Value("topic")
+}
